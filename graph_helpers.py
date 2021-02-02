9524ddcfdd5c022e64478b8197990645e2e0e0b0
Jay Soni <jbsoni@uwaterloo.ca>
2021-02-02 02:46:20 -0500

    Implemented the brute force soln to the weighted max cut problem to do:     - figure out how to plot edges with labels properly     - implement the QAOA soln to the weighted max cut problem

--- graph_helpers.py
+++ graph_helpers.py
@@ -1,12 +1,12 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 
 
 def random_graph_gen(max_num_nodes=5, max_num_edges=10, seed=None):
     """
     random graph generator produces a networkx.graph object
-
     :param max_num_nodes: int, max number of nodes in the graph
     :param max_num_edges: int, max number of edges in the graph
     :param seed: int, random seed
@@ -26,9 +26,59 @@ def random_graph_gen(max_num_nodes=5, max_num_edges=10, seed=None):
     return g
 
 
-def brute_force_soln(graph):
-    
-    return set_a
+def zero_pad(bit_string, max_len):
+    final_bit_string = bit_string
+
+    while len(final_bit_string) < max_len:
+        final_bit_string = '0' + final_bit_string
+
+    return final_bit_string
+
+
+def brute_force_soln(graph, debug=False):
+    """
+    Compute the maximum cut for a weighted graph using a brute force search
+    :param graph: nx.graph object, the graph we want to cut
+    :param debug: bool, if true adds print statements (default = False)
+    :return: tuple (float, list), the magnitude of the cut, and one group of nodes, (the rest are in other group)
+    """
+    node_lst = graph.nodes()
+    num_nodes = len(node_lst)
+    if debug:
+        print(node_lst)
+        print(num_nodes)
+
+    """
+    We want to split the the nodes into 2 groups, such that we maximize the weights between
+    the two groups. We can assign to each node a bit (0, 1) which represents the group it is in. 
+    Thus we can use this to assign a unique bit string to each cut. I make use of this below: 
+    """
+
+    max_bin = 2**(num_nodes - 1)
+    optimal_cut_size = None
+    optimal_group_of_nodes = []
+
+    for num in range(0, max_bin + 1):
+        bit_string = str(np.base_repr(num, base=2))
+        bit_string = zero_pad(bit_string, num_nodes)
+        if debug:
+            print('working on: ' + bit_string)
+
+        group_of_nodes = []
+        for index, digit in enumerate(bit_string):
+            if digit == '1':
+                group_of_nodes.append(index)
+
+        cut_size = nx.cut_size(graph, group_of_nodes, weight='weight')
+
+        if (num == 0) or (cut_size > optimal_cut_size):
+            optimal_cut_size = cut_size
+            optimal_group_of_nodes = group_of_nodes
+
+        if debug:
+            print(' --> cutsize = {}'.format(cut_size))
+
+    return optimal_cut_size, optimal_group_of_nodes
 
 
 def main():
@@ -42,6 +92,9 @@ def main():
     # nx.draw_networkx_edge_labels(g, nx.random_layout(g), labels=labels)
     nx.draw(g, with_labels=True)
     plt.show()
+    cut, group = brute_force_soln(g, debug=True)
+    print('************\n Optimal cut = {}'.format(cut))
+    print('Optimal group = ', group)
     return
 
 
